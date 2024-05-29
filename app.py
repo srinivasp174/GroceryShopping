bc18330a54cbd6da7e56c0acabfa0055abe3051a
srinivasp174 <nt5srinivaspeethala@gmail.com>
2024-05-29 21:11:37 +0530

    added sql tables, split the app into models, config, routes, and added some basic styling to login and registration pages

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
+app = Flask(__name__)
 
-app =Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html', name = "Srinivas")
+import config
+import models
+import routes
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,54 @@
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    userid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True, nullable=False)
+    passhash = db.Column(db.String(256), nullable=False)
+    name = db.Column(db.String(64), nullable=False)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    
+class Category(db.Model):
+    categoryid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), unique=True, nullable=False)
+    description = db.Column(db.String(256), nullable=True)
+    
+    products = db.relationship('Product', backref='category', lazy=True)
+    
+class Product(db.Model):
+    productid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(256), nullable=True)
+    categoryid = db.Column(db.Integer, db.ForeignKey('category.categoryid'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    manufacture = db.Column(db.Date, nullable=False)
+    
+    carts = db.relationship('Cart', backref='product', lazy=True)
+    orders = db.relationship('Order', backref='product', lazy=True)
+    
+class Cart(db.Model):
+    cartid = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, db.ForeignKey('user.userid'), nullable=False)
+    productid = db.Column(db.Integer, db.ForeignKey('product.productid'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    
+class Transaction(db.Model):
+    transactionid = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, db.ForeignKey('user.userid'), nullable=False)
+    datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    
+    orders = db.relationship('Order', backref='transaction', lazy=True)
+    
+class Order(db.Model):
+    orderid = db.Column(db.Integer, primary_key=True)
+    transactionid = db.Column(db.Integer, db.ForeignKey('transaction.transactionid'), nullable=False)
+    productid = db.Column(db.Integer, db.ForeignKey('product.productid'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    
+with app.app_context():
+    db.create_all()
